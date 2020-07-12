ae868d45ad67aaf195cb3ce08b40be599f644cb8
Ju Mong Ko <jk2299@cornell.edu>
2020-07-13 01:41:47 +0900

    router + navigation

---
+++ src/routers/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.css'
+
+function About(){
+    return (
+        <div className="about_container">
+            <span>
+                Hi -
+            </span>
+            <span>
+                George Orwell
+            </span>
+        </div>
+    );
+}
+
+export default About;
